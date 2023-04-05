e32d29bc3f0803c4bc8f21d11b1f891f5851a746
itsuro <itsuro2@icloud.com>
2023-04-05 18:36:40 +0900

    change of position

--- Main/urls.py
+++ Main/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('video/', include('video.urls')),
+    # path('blog/', include('blog.urls')),
+
     path('', include('blog.urls')),
 ]
