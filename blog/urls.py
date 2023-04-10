602f999aa30136f9c7cc9b6692718de76e491ad6
itsuro <itsuro2@icloud.com>
2023-04-10 18:22:31 +0900

    blog-update

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.frontpage, name='frontpage'),
-    path('<slug:slug>/', views.post_detail, name="post_detail"),
+    path('video/', include('video.urls')),
+    path('', views.index, name="index"),
+    path('frontpage/', views.frontpage, name='frontpage'),
+
+    path('create/', views.post_create, name="post_create"),
+    path('<int:pk>/', views.post_detail, name="post_detail"),
+    path('<int:pk>/post_Change_Delete', views.post_Change_Delete,
+         name="post_Change_Delete"),
 ]
