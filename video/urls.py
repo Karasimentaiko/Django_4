24e0e5581110326eb41031a17fa1740e750333dd
itsuro <itsuro2@icloud.com>
2023-04-10 18:25:46 +0900

    Main-update

--- video/urls.py
+++ video/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('upload/', views.upload, name='upload'),
     path('success/', views.success, name='success'),
+    # path('blog/', include('blog.urls')),
 ]
